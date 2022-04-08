a539b89b43c5382bf07a6f82f02efc9e6aa234cd
dymafr <erwan1309@hotmail.com>
2022-04-08 11:45:05 +0200

    1 file deleted

--- src/provideKeys.ts
+++
@@ -1,7 +0,0 @@
-import type { InjectionKey } from 'vue';
-import type { AuthorInterface } from './Author.interface';
-
-export const authorKey = Symbol() as InjectionKey<{
-  data: AuthorInterface;
-  updateBirthday: () => void;
-}>;
