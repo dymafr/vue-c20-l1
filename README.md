5bd5ebb919f39d37296e9d03384450d2d959e7d4
dymafr <erwan1309@hotmail.com>
2022-04-08 11:32:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-ovdqsc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-ovdqsc)
---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="p-20">
+    <div class="mb-20 d-flex w100 justify-content-center align-items-center">
+      <button class="btn btn-primary mr-20" @click="add">Ajouter</button>
+      <button class="btn btn-primary" @click="remove">Enlever</button>
+    </div>
+    <div class="container">
+      <TransitionGroup name="list" tag="ul">
+        <li class="mb-10 card" v-for="item in items" :key="item">
+          {{ item }}
+        </li>
+      </TransitionGroup>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let nextNum = ref(10);
+const items = ref<number[]>([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+function randomIndex() {
+  return Math.floor(Math.random() * items.value.length);
+}
+
+function add() {
+  items.value.splice(randomIndex(), 0, nextNum.value++);
+}
+
+function remove() {
+  items.value.splice(randomIndex(), 1);
+}
+</script>
+
+<style lang="scss">
+@import './assets/scss/base.scss';
+
+li {
+  cursor: pointer;
+  width: 100%;
+}
+
+.container {
+  position: relative;
+  padding: 0;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+.list-move,
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-leave-active {
+  position: absolute;
+}
+</style>
